08869613c2e8f794cf8481229f647844f6f3d9a4
emil <emil>
2026-03-18 13:35:44 +0500

    Cashier

--- cashier.py
+++ cashier.py
@@ -1,10 +1,27 @@
 class Cashier:
-
     def __init__(self):
-        self.coins = [1, 5, 10, 25, 50]
-    
+        self.coins = [50, 25, 10, 5, 1] 
+
     def calculate_change(self, amount_paid, item_price):
-        raise NotImplementedError()
+        change = amount_paid - item_price
+        change_coins = []
+        
+        for coin in self.coins:
+            while change >= coin:
+                change -= coin
+                change_coins.append(coin)
+        
+        return change_coins
+
     
     def budget_options(self, budget, current_items):
-        raise NotImplementedError()
+        all_possible_combinations = []
+        for i in range(1, len(current_items) + 1):
+            for combo in combinations(current_items, i):
+                total_price = sum(item[1] for item in combo)
+                if total_price <= budget:
+                    all_possible_combinations.append((combo, total_price))
+
+        for combo, price in all_possible_combinations:
+            names = [current_items[0] for item in combo]
+            print(f"Набор{','.join(names)}, Итог{price}")
